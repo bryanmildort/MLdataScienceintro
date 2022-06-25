f8863c6fce1807610d9f5d455af02568de87e797
Bryan Mildort <59807530+bryanmildort@users.noreply.github.com>
2022-06-24 21:10:53 -0400

    cleanup

--- Stock Price Predictor/testing.py
+++
@@ -1,6 +0,0 @@
-    self.pushButton.clicked.connect(self.predict) # adding function to Predict! button
-
-
-    def predict(self): # predict function
-        text = self.lineEdit.text()
-        print()
